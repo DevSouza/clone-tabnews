18066f9ec6606b197afd6630b2750f090fcea7ba
Andre Souza <devsouza01@gmail.com>
2023-11-18 18:58:21 +0000

    adiciona pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+function Home() {
+    return (
+        <>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNSCCFTaDQ_ovBKJl1rmbywKoXnPzUzCEdWA&usqp=CAU"/>
+            <h2>Debora Swan</h2>
+        </>
+    )
+}
+
+export default Home;

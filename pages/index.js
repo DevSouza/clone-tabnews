28f468f3f54937d1d94057a93869afef9aff92a5
Andre Souza <devsouza01@gmail.com>
2023-11-23 23:38:33 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,14 @@
 function Home() {
-    return (
-        <>
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNSCCFTaDQ_ovBKJl1rmbywKoXnPzUzCEdWA&usqp=CAU"/>
-            <h2>Debora Swan</h2>
-        </>
-    )
+  return (
+    <>
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNSCCFTaDQ_ovBKJl1rmbywKoXnPzUzCEdWA&usqp=CAU" />
+      <h2>Debora Swan</h2>
+    </>
+  );
+}
+
+function teste2() {
+  console.log(`teste`);
 }
 
 export default Home;

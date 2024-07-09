930b719ef80b11194dec843444d51bf73111bf84
Andre Souza <devsouza01@gmail.com>
2024-07-09 13:22:22 +0000

    fix `eslint` linting

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,12 @@
+import Image from "next/image";
+
 function Home() {
   return (
     <>
-      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNSCCFTaDQ_ovBKJl1rmbywKoXnPzUzCEdWA&usqp=CAU" />
+      <Image
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNSCCFTaDQ_ovBKJl1rmbywKoXnPzUzCEdWA&usqp=CAU"
+        alt="Imagem de um personagem de anime com coração no lugar dos olhos."
+      />
       <h2>Debora Swan</h2>
     </>
   );

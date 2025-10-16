b8349571468d33eecfa6e77ce7b8b0c4f1549c2a
Andre Souza <devsouza01@gmail.com>
2025-10-16 22:36:58 +0000

    feat: send activation email after `user` registration

---
+++ infra/migrations/1760651842292_create-user-activation-tokens.js
@@ -0,0 +1,37 @@
+exports.shorthands = undefined;
+
+exports.up = (pgm) => {
+  pgm.createTable("user_activation_tokens", {
+    id: {
+      type: "uuid",
+      primaryKey: true,
+      default: pgm.func("gen_random_uuid()"),
+    },
+    used_at: {
+      type: "timestamptz",
+      notNull: false,
+    },
+    user_id: {
+      type: "uuid",
+      notNull: false,
+    },
+    expires_at: {
+      type: "timestamptz",
+      notNull: true,
+    },
+
+    created_at: {
+      type: "timestamptz",
+      notNull: true,
+      default: pgm.func("timezone('UTC', now())"),
+    },
+
+    updated_at: {
+      type: "timestamptz",
+      notNull: true,
+      default: pgm.func("timezone('UTC', now())"),
+    },
+  });
+};
+
+exports.down = false;
--- pages/api/v1/users/index.js
+++ pages/api/v1/users/index.js
@@ -2,6 +2,7 @@ import { createRouter } from "next-connect";
 import controller from "infra/controller.js";
 
 import user from "models/user.js";
+import activation from "models/activation";
 
 const router = createRouter({});
 
@@ -12,5 +13,9 @@ export default router.handler(controller.errorHandlers);
 async function postHandler(request, response) {
   const userInputValues = request.body;
   const newUser = await user.create(userInputValues);
+
+  const activationToken = await activation.create(newUser.id);
+  await activation.sendEmailToUser(newUser, activationToken);
+
   return response.status(201).json(newUser);
 }

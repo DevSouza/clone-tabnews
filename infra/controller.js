4429b954b9921ce1e8999413aa7c2dbcc4f285cf
Andre Souza <devsouza01@gmail.com>
2025-11-13 19:27:53 +0000

    feat: create `authorization` model and use it inside `/sessions` controller

--- infra/controller.js
+++ infra/controller.js
@@ -10,6 +10,7 @@ import {
 import * as cookie from "cookie";
 import session from "models/session";
 import user from "models/user";
+import authorization from "models/authorization";
 
 function onNoMatchHandler(request, response) {
   const publicErrorObject = new MethodNotAllowedError();
@@ -97,7 +98,7 @@ function canRequest(feature) {
   return function canRequestMiddleware(request, response, next) {
     const userTryingToRequest = request.context.user;
 
-    if (userTryingToRequest.features.includes(feature)) {
+    if (authorization.can(userTryingToRequest, feature)) {
       return next();
     }
 
--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -5,6 +5,7 @@ import database from "infra/database.js";
 import migrator from "models/migrator.js";
 import user from "models/user.js";
 import session from "models/session";
+import activation from "models/activation";
 
 const emailHttpUrl = `http://${process.env.EMAIL_HTTP_HOST}:${process.env.EMAIL_HTTP_PORT}`;
 
@@ -93,6 +94,10 @@ function extractUUID(text) {
   return match ? match[0] : null;
 }
 
+async function activateUser(inactiveUser) {
+  return await activation.activateUserByUserId(inactiveUser.id);
+}
+
 const orcherstrator = {
   waitForAllServices,
   clearDatabase,
@@ -102,6 +107,7 @@ const orcherstrator = {
   deleteAllEmails,
   getLastEmail,
   extractUUID,
+  activateUser,
 };
 
 export default orcherstrator;
